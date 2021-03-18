a10915ddbe6a81fa5e462bf66e291e7f2d2ddb5d
Sean O'Hara <sohara@sohara.com>
2021-03-18 12:39:53 -0400

    Repro case for npm dependencies breaking scripts

--- packages/example/src/pages/index.client.tsx
+++ packages/example/src/pages/index.client.tsx
@@ -1,4 +1,9 @@
-import initWidgets from 'next-client-script/dist/initWidgets';
-import Counter from 'components/Counter/Counter.client';
+import initWidgets from "next-client-script/dist/initWidgets";
+import Counter from "components/Counter/Counter.client";
+import { gte } from "lodash";
+
+console.log("hello");
+const answer = gte(2, 5);
+console.log({ answer });
 
 initWidgets([Counter]);
